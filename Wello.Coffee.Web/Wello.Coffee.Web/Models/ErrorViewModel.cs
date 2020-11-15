6dd7385bc61e1597b87307541f5a8342e292cd60
euan.murray <euan.murray@itftennis.com>
2020-11-15 12:34:43 -0500

    Created initial web project

---
+++ Wello.Coffee.Web/Wello.Coffee.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wello.Coffee.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
